76b29d4e0e5ac1026828f1894cab5a33be672e4e
Alexander Medvetsky <medvetsky.working@gmail.com>
2024-06-17 21:53:52 +0300

    Changes after code review

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -8,8 +8,8 @@ func (r *Repository) AddURL(longURL []byte, shortenedURL string) {
 	r.MapURL[shortenedURL] = longURL
 }
 
-func (stC *Repository) FindShortenedURL(shortenedURL string) ([]byte, bool) {
-	longURL := stC.MapURL[shortenedURL]
+func (r *Repository) FindShortenedURL(shortenedURL string) ([]byte, bool) {
+	longURL := r.MapURL[shortenedURL]
 	found := true
 	if longURL == nil {
 		found = false
