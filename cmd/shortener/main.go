03ff1c3a010046bada191a0bd1e35c4c1624b178
Alexander Medvetsky <medvetsky.working@gmail.com>
2024-07-03 19:33:47 +0300

    Code increment #8

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/AxMdv/go-url-shortener/internal/app/compress"
 	"github.com/AxMdv/go-url-shortener/internal/app/config"
 	"github.com/AxMdv/go-url-shortener/internal/app/handlers"
 	"github.com/AxMdv/go-url-shortener/internal/app/logger"
@@ -21,9 +22,9 @@ func main() {
 		fmt.Println("Failed to init logger")
 	}
 	r := chi.NewRouter()
-	r.Post("/", logger.WithLogging(s.CreateShortURL))
-	r.Get("/{shortenedURL}", logger.WithLogging(s.GetLongURL))
-	r.Post("/api/shorten", logger.WithLogging(s.CreateShortURLJson))
+	r.Post("/", logger.WithLogging(compress.GzipMiddleware(s.CreateShortURL)))
+	r.Get("/{shortenedURL}", logger.WithLogging(compress.GzipMiddleware(s.GetLongURL)))
+	r.Post("/api/shorten", logger.WithLogging(compress.GzipMiddleware(s.CreateShortURLJson)))
 
 	log.Fatal(http.ListenAndServe(config.Options.RunAddr, r))
 }
---
+++ internal/app/compress/gzip.go
@@ -0,0 +1,114 @@
+package compress
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type compressWriter struct {
+	w  http.ResponseWriter
+	zw *gzip.Writer
+}
+
+func newCompressWriter(w http.ResponseWriter) *compressWriter {
+	return &compressWriter{
+		w:  w,
+		zw: gzip.NewWriter(w),
+	}
+}
+
+func (c *compressWriter) Header() http.Header {
+	return c.w.Header()
+}
+
+// Compress only if Content-Type is "text/html" or "application/json"
+func (c *compressWriter) shouldCompress() bool {
+	contentType := c.Header().Get("Content-Type")
+	if contentType == "text/html" || contentType == "application/json" {
+		return true
+	}
+	return false
+}
+func (c *compressWriter) Write(p []byte) (int, error) {
+	if c.shouldCompress() {
+		return c.zw.Write(p)
+	}
+	return c.w.Write(p)
+}
+
+func (c *compressWriter) WriteHeader(statusCode int) {
+	if statusCode < 400 && c.shouldCompress() {
+		c.w.Header().Set("Content-Encoding", "gzip")
+	}
+	c.w.WriteHeader(statusCode)
+}
+
+// Close закрывает gzip.Writer и досылает все данные из буфера.
+func (c *compressWriter) Close() error {
+	return c.zw.Close()
+}
+
+// compressReader реализует интерфейс io.ReadCloser и позволяет прозрачно для сервера
+// декомпрессировать получаемые от клиента данные
+type compressReader struct {
+	r  io.ReadCloser
+	zr *gzip.Reader
+}
+
+func newCompressReader(r io.ReadCloser) (*compressReader, error) {
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &compressReader{
+		r:  r,
+		zr: zr,
+	}, nil
+}
+
+func (c compressReader) Read(p []byte) (n int, err error) {
+	return c.zr.Read(p)
+}
+
+func (c *compressReader) Close() error {
+	if err := c.r.Close(); err != nil {
+		return err
+	}
+	return c.zr.Close()
+}
+
+func GzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ow := w
+
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// меняем тело запроса на новое
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		// передаём управление хендлеру
+		h.ServeHTTP(ow, r)
+	}
+}
