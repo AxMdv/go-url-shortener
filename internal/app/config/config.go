8c00e8e5e863860127f9518e765c9ba867bd504b
Alexander Medvetsky <medvetsky.working@gmail.com>
2024-07-04 19:19:41 +0300

    Initial commit

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -8,11 +8,13 @@ import (
 var Options struct {
 	RunAddr            string
 	ResponseResultAddr string
+	FileStorage        string
 }
 
 func ParseOptions() {
 	flag.StringVar(&Options.RunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&Options.ResponseResultAddr, "b", "http://localhost:8080", "resut basic response address (before shortened URL)")
+	flag.StringVar(&Options.FileStorage, "f", "/tmp/short-url-db.json", "path to save shortened URLs")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
@@ -20,5 +22,8 @@ func ParseOptions() {
 	}
 	if envResultAddr := os.Getenv("BASE_URL"); envResultAddr != "" {
 		Options.ResponseResultAddr = envResultAddr
+	}
+	if envFileStorage := os.Getenv("FILE_STORAGE_PATH"); envFileStorage != "" {
+		Options.FileStorage = envFileStorage
 	}
 }
