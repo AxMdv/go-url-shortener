6af4a7468a60431fb26c3561a10acb88071992ec
Alexander Medvetsky <medvetsky.working@gmail.com>
2024-10-14 00:25:18 +0300

    added build info output

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/AxMdv/go-url-shortener/internal/app"
 )
 
+// go run -ldflags "-X main.buildVersion=v1.0.1 -X 'main.buildDate=$(date +'%Y/%m/%d %H:%M:%S')' -X main.buildCommit=hello world" main.go
+// go run -ldflags "-X main.buildVersion=v1.0.1 -X main.buildCommit=hello-world" main.go -f=""
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	fmt.Printf("Build version:%s\n", formatValue(buildVersion))
+	fmt.Printf("Build date:%s\n", formatValue(buildDate))
+	fmt.Printf("Build commit:%s\n", formatValue(buildCommit))
+}
+
+func formatValue(buildData string) string {
+	if buildData == "" {
+		return "N/A"
+	}
+	return buildData
+}
+
 func main() {
+
+	printBuildInfo()
 
 	a, err := app.NewApp()
 	if err != nil {
