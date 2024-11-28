697d737d7bf456ca6fa657cbd0942e53158b4e84
Alexander Medvetsky <medvetsky.working@gmail.com>
2024-11-28 19:00:26 +0300

    added graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,8 +2,13 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-chi/chi/v5"
 
@@ -25,6 +30,8 @@ type App struct {
 	ShortenerHandlers *handlers.ShortenerHandlers
 
 	router *chi.Mux
+
+	server *http.Server
 }
 
 // NewApp creates a new app of a URL shortener
@@ -46,26 +53,74 @@ func NewApp(config *config.Options) (*App, error) {
 
 	router := handlers.NewShortenerRouter(shortenerHandlers)
 
+	srv := &http.Server{
+		Addr:    config.RunAddr,
+		Handler: router,
+	}
+
 	a := &App{
 		urlService:        urlService,
 		urlRepository:     repository,
 		configOptions:     config,
 		ShortenerHandlers: shortenerHandlers,
 		router:            router,
+		server:            srv,
 	}
 	return a, nil
 }
 
 // Run is a main process of working application
 func (a *App) Run() error {
-	return a.runHTTPServer()
+	idleConnsClosed := make(chan struct{})
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	go a.processInterrupt(sigint, idleConnsClosed)
+
+	if err := a.runHTTPServer(); err != http.ErrServerClosed {
+		return err
+	}
+
+	<-idleConnsClosed
+	err := a.gracefullShutdown()
+	if err != nil {
+		fmt.Print(err)
+	}
+	return err
 }
 
 func (a *App) runHTTPServer() error {
+
 	if a.configOptions.EnableHTTPS {
 		log.Printf("HTTPS server is running on %s", a.configOptions.RunAddr)
-		return http.ListenAndServeTLS(a.configOptions.RunAddr, "./certs/certbundle.pem", "./certs/server.key", a.router)
+		return a.server.ListenAndServeTLS("./certs/certbundle.pem", "./certs/server.key")
 	}
 	log.Printf("HTTP server is running on %s", a.configOptions.RunAddr)
-	return http.ListenAndServe(a.configOptions.RunAddr, a.router)
+	return a.server.ListenAndServe()
+}
+
+func (a *App) processInterrupt(sigint chan os.Signal, idleConnsClosed chan struct{}) {
+	<-sigint
+	if err := a.server.Shutdown(context.Background()); err != nil {
+		// ошибки закрытия Listener
+		log.Printf("HTTP server Shutdown: %v", err)
+	} else {
+		log.Println("successfully stopped http server")
+	}
+	close(idleConnsClosed)
+}
+
+func (a *App) gracefullShutdown() error {
+	// close repo if it has method close()
+	log.Println("trying to close repository..")
+	closerRepo, ok := a.urlRepository.(service.Closer)
+	var err error
+	if ok {
+		err = closerRepo.Close()
+		if err == nil {
+			log.Println("success in closing repo")
+		}
+	}
+	return err
 }
